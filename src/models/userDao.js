b4013c097285d2d1a60bf43bb9e38dc110040e94
이명석 <yhkyhk92@gmail.com>
2022-11-17 15:00:44 +0900

    Refactor : userDao getUserByNickname 함수 query 수정

--- src/models/userDao.js
+++ src/models/userDao.js
@@ -3,10 +3,10 @@ const { appDataSource } = require('./dataSource');
 const createUser = async (name, nickname, password, birth) => {
   await appDataSource.query(
     `INSERT INTO users(
-    name,
-    nickname,
-    password,
-    birth
+      name,
+      nickname,
+      password,
+      birth
     ) VALUES (?, ?, ?, ?);
     `,
     [name, nickname, password, birth]
@@ -15,7 +15,11 @@ const createUser = async (name, nickname, password, birth) => {
 
 const getUserByNickname = async (nickname) => {
   const [user] = await appDataSource.query(
-    `SELECT *
+    `SELECT
+      name,
+      nickname,
+      password,
+      birth
     FROM users
     WHERE nickname = ?
     `,
