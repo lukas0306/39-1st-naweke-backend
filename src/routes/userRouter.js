298aa5a3a088637e8f9081d9c047ff70c5b97220
root <root@DESKTOP-B9G676P>
2022-11-18 14:30:20 +0900

    router changed

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const userController = require('../controlloers/userController');
+const userController = require('../controllers/userController');
 
 const userRouter = express.Router();
 
