867b4aa0575127718aa5b17a9ae4354c3ee79d45
이명석 <yhkyhk92@gmail.com>
2022-11-18 11:28:56 +0900

    Modify : products 테이블 수정사항 통합

--- db/migrations/20221117074658_modify_table_products.sql
+++ db/migrations/20221117074658_modify_table_products.sql
@@ -1,5 +1,7 @@
 -- migrate:up
-ALTER TABLE `products` ADD `desc` varchar(255) NOT NULL AFTER `name`
+ALTER TABLE `products` ADD `desc` varchar(255) NOT NULL AFTER `name`;
+ALTER TABLE products MODIFY COLUMN gender_id INT NULL
+
 
 -- migrate:down
 
--- db/migrations/20221117090643_modify_table_products2.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-ALTER TABLE products MODIFY COLUMN gender_id INT NULL
-
--- migrate:down
-
