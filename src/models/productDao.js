ffa020356d0b7568a81f664e500ce36dcafdfd5b
이명석 <yhkyhk92@gmail.com>
2022-11-21 18:06:40 +0900

    [Refactor] desc 변수명 오류로 인한 쿼리 수정

--- src/models/productDao.js
+++ src/models/productDao.js
@@ -3,14 +3,14 @@ const { appDataSource } = require('./dataSource');
 const readProductInfo = async (productId) => {
   const productInfo = await appDataSource.query(
     `
-    SELECT
+  SELECT
     p.name name,
-    p.desc desc,
+    p.desc description,
     p.thumbnail_image_url image,
     po.price price,
     JSON_ARRAYAGG(po.size_id) size
-  FROM products AS p
-  LEFT JOIN product_options AS po
+  FROM products p
+    LEFT JOIN product_options po
     ON p.id = po.product_id
   WHERE p.id = ?
   GROUP BY p.name, p.desc, p.thumbnail_image_url, po.price
@@ -21,3 +21,4 @@ const readProductInfo = async (productId) => {
 };
 
 module.exports = { readProductInfo };
+// p.desc desc,
