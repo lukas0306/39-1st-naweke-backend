b5fe469db88522c1609c683db0bcb74a0bd68ea7
이명석 <yhkyhk92@gmail.com>
2022-11-21 16:24:54 +0900

    [refactor] 주문내역 GET 요청 엔드포인트 쿼리 수정

--- src/models/orderDao.js
+++ src/models/orderDao.js
@@ -1,19 +1,28 @@
 const { appDataSource } = require('./dataSource');
 
 const readOrderList = async (userId) => {
-  const [orderList] = await appDataSource.query(
+  const orderList = await appDataSource.query(
     `SELECT
-    o.id,
-    o.order_status_id,
-    o.user_id,
-    o.total_price,
-    oi.order_item_status_id,
-    oi.product_option_id,
-    oi.quantity
+    o.id orderId,
+    o.order_status_id orderStatusId,
+    o.total_price totalPrice,
+      JSON_ARRAYAGG(
+      JSON_OBJECT(
+      "productName", p.name,
+      "quantity", oi.quantity,
+      "orderItemStatus", oi.order_item_status_id,
+      "productOptionId", oi.product_option_id,
+      "thumbnailImage", p.thumbnail_image_url
+    )) orderProduct
     FROM orders o
-      LEFT JOIN order_items oi
-      ON o.id = oi.order_id
-      WHERE o.user_id = ?`,
+    LEFT JOIN order_items oi
+    ON o.id = oi.order_id
+    LEFT JOIN product_options po
+    ON po.id = oi.product_option_id
+    LEFT JOIN products p
+    ON po.product_id = p.id
+    WHERE o.user_id = ?
+    GROUP BY o.id`,
     [userId]
   );
   return orderList;
--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const orderController = require('../controlloers/orderController');
 
+const { validateAccessToken } = require('../middlewares/validateAccessToken');
+
 const orderRouter = express.Router();
 
-orderRouter.get('/', orderController.getOrderList);
+orderRouter.get('/', validateAccessToken, orderController.getOrderList);
 
 module.exports = { orderRouter };
