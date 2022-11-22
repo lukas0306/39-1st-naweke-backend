bbbb0617e495c236b7b566488f82149d17aa4106
이명석 <yhkyhk92@gmail.com>
2022-11-23 00:11:52 +0900

    [Modify] reviews 평점 및 image url 추가

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,14 @@
 const { readProductInfo } = require('../services/productService');
 
+const getAllProducts = async (req, res) => {
+  try {
+    const productsData = await productService.getProductList(req.query);
+    res.status(200).json({ data: productsData });
+  } catch (err) {
+    res.status(err.statusCode || 400).json({ message: err.message });
+  }
+};
+
 const loadProductInfo = async (req, res, next) => {
   try {
     const { productId } = req.params;
@@ -10,4 +19,4 @@ const loadProductInfo = async (req, res, next) => {
   }
 };
 
-module.exports = { loadProductInfo };
+module.exports = { loadProductInfo, getAllProducts };
