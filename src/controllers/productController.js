aa477faa854f15cf8d08d1e2ca76a8d908f5d2ba
이명석 <yhkyhk92@gmail.com>
2022-11-23 15:03:55 +0900

    [Refactor] getAllProduct 함수 삭제

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -10,13 +10,4 @@ const loadProductInfo = async (req, res, next) => {
   }
 };
 
-const getAllProducts = async (req, res) => {
-  try {
-    const productsData = await productService.getProductList(req.query);
-    res.status(200).json({ data: productsData });
-  } catch (err) {
-    res.status(err.statusCode || 400).json({ message: err.message });
-  }
-};
-
-module.exports = { loadProductInfo, getAllProducts };
+module.exports = { loadProductInfo };
