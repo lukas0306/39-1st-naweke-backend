e48138dc335423d891681d7c73e4ef900f865e04
이명석 <yhkyhk92@gmail.com>
2022-11-23 16:21:05 +0900

    [Refactor] getAllProduct 레이어드 아키텍쳐 점검

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,14 +1,5 @@
 const { readOrderList } = require('../services/orderService');
 
-const getAllProducts = async (req, res) => {
-  try {
-    const productsData = await productService.getProductList(req.query);
-    res.status(200).json({ data: productsData });
-  } catch (err) {
-    res.status(err.statusCode || 400).json({ message: err.message });
-  }
-};
-
 const getOrderList = async (req, res, next) => {
   try {
     const userId = req.decoded;
@@ -19,4 +10,4 @@ const getOrderList = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllProducts, getOrderList };
+module.exports = { getOrderList };
